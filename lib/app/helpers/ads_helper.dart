156f0b79d51f7d383aea61b638acd1ab809575c9
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2021-04-15 18:08:12 -0300

    Added correct ad ids in ads_helper

--- lib/app/helpers/ads_helper.dart
+++ lib/app/helpers/ads_helper.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 class AdsHelper {
   static String get appId {
     if (Platform.isAndroid) {
+      // This is the app id, gotten from: https://apps.admob.com/v2/apps/2537348407/settings
       return "ca-app-pub-1583793484936118~2537348407";
     } else if (Platform.isIOS) {
       return "<YOUR_IOS_ADMOB_APP_ID>";
@@ -36,7 +37,7 @@ class AdsHelper {
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
     // When deploying, use the right one: https://apps.admob.com/v2/apps/2537348407/adunits/list?pli=1
-      return "ca-app-pub-3940256099942544/8691691433";
+      return "ca-app-pub-1583793484936118/2252856498";
     } else if (Platform.isIOS) {
       return "<YOUR_IOS_INTERSTITIAL_AD_UNIT_ID>";
     } else {
